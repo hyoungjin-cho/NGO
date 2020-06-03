5e49b2a53a141b7c8dfb0b237a4002d7daa3c5c8
hyoungjin-cho <tim.asset90@gmail.com>
2020-06-03 12:20:01 -0700

    added user entity

---
+++ NGO/src/main/java/com/summitworks/ngo/entities/User.java
@@ -0,0 +1,32 @@
+package com.summitworks.ngo.entities;
+
+public class User {
+	String firstname;
+	String lastname;
+	String email;
+	String role;
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
