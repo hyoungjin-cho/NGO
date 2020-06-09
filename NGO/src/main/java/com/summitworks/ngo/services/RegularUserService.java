324242cb9416ac886d7932b811721a60a1d8cf84
hyoungjin-cho <tim.asset90@gmail.com>
2020-06-08 18:14:42 -0700

    merging

--- NGO/src/main/java/com/summitworks/ngo/services/RegularUserService.java
+++
@@ -1,9 +0,0 @@
-package com.summitworks.ngo.services;
-
-import com.summitworks.ngo.entities.Event;
-import com.summitworks.ngo.entities.RegularUser;
-
-public interface RegularUserService {
-
-	void registerEvent(RegularUser u, Event e);
-}
